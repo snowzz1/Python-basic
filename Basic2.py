f8d99853dde478088d8057e403898c8ac1fc54b2
snowzz1 <jacare1888@gmail.com>
2024-08-30 21:22:28 -0300

    first commit

---
+++ Basic2.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+print(nome)
+print(idade)
+print(peso)
---
+++ Basic3.py
@@ -0,0 +1,7 @@
+nome = "Programador Python"
+idade = 19
+peso = 78
+
+print(nome)
+print(idade)
+print(peso)
